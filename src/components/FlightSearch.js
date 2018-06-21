712e81768caf87a6f623530d35bd892ca0630880
Stefan Cameron <stefan@stefcameron.com>
2018-06-21 12:03:45 +0100

    Session feedback

--- src/components/FlightSearch.js
+++ src/components/FlightSearch.js
@@ -8,22 +8,42 @@ export default class extends PureComponent {
     constructor(props) {
         super(props);
 
-        this.onInputChange = this.onInputChange.bind(this);
+        /*
+        TODO: Initialize the state property here; initialize the string values to
+        empty strings, and the date values to null.
+        */
+
+        this.onOriginChange = this.onOriginChange.bind(this);
+        this.onDestinationChange = this.onDestinationChange.bind(this);
         this.departureDateChange = this.departureDateChange.bind(this);
         this.returnDateChange = this.returnDateChange.bind(this);
         this.onSearchFlights = this.onSearchFlights.bind(this);
     }
 
-    onInputChange(e) {
+    onOriginChange(e) {
+        /* TODO: capture origin and destination airport codes */
+    }
+
+    onDestinationChange(e) {
         /* TODO: capture origin and destination airport codes */
     }
 
     departureDateChange(date) {
-        /* TODO: capture departure date */
+        /*
+        TODO: capture departure date.
+        The 'date' argument is a moment.js object that has a .format() method
+        to use to get the selected date in any format, e.g. 'YYYY-MM-DD' which
+        the API will want later: date.format('YYYY-MM-DD')
+        */
     }
 
     returnDateChange(date) {
-        /* TODO: capture return date */
+        /*
+        TODO: capture return date.
+        The 'date' argument is a moment.js object that has a .format() method
+        to use to get the selected date in any format, e.g. 'YYYY-MM-DD' which
+        the API will want later: date.format('YYYY-MM-DD')
+        */
     }
 
     onSearchFlights(event) {
